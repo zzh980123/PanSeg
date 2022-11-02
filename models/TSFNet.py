1d7ba35b9328974cdb8fe87314eb278be000448d
zzh980123 <291940676@qq.com>
2022-11-02 15:32:14 +0800

    add flow

--- models/TSFNet.py
+++ models/TSFNet.py
@@ -1,12 +1,84 @@
+import math
+
+import monai.networks.nets
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from monai.networks.nets import *
+from einops import rearrange
+import flow
 
 class TSFNet(nn.Module):
-    def __init__(self, in_channels=1, out_channels=1, branch_nums=3, depth=[0, 0, 0, 0], dims=[32, 64, 128, 256], backbone=None):
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.branch_nums = branch_nums
+    def __init__(self, device=None, in_channels=1, out_channels=4, max_scaling=4):
+        super(TSFNet, self).__init__()
+        self.head = monai.networks.nets.UNet(
+            spatial_dims=2,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            channels=(16, 32, 64, 128, 256),
+            strides=(2, 2, 2, 2),
+            num_res_units=2
+        )
+        self.max_scaling = max_scaling
+        self.min_scaling = 1 / max_scaling
+        assert self.max_scaling > 0
+        self.M = math.log2(self.max_scaling)
+        
+        self.max_pooling = nn.AdaptiveMaxPool2d(1, return_indices=True)
+
+    def get_st_flows(self, shape_, device):
+
+        if self.dyn_flow_shape:
+            self.register_buffer('s_flow', nn.Parameter(flow.gen_flow_scale(shape_[2:]), requires_grad=False))
+            self.register_buffer('t_flow', nn.Parameter(flow.gen_flow_transport(shape_[2:]), requires_grad=False))
+            self.trans = flow.SpatialTransformer(shape_[2:], mode='nearest')
+
+            self.s_flow = self.s_flow.to(device)
+            self.t_flow = self.t_flow.to(device)
+            self.dyn_flow_shape = False
+
+        return self.s_flow, self.t_flow
+
+    def forward(self, x):
+        ori_shape = x.shape
+        feature = self.head(x)
+        feature = torch.sigmoid(feature)
+        
+        b, c, h, w = feature.shape
+        _, max_index = self.max_pooling(feature[:, 3:, ...])  # 最后一个通道的最大值序号
+        max_index = torch.repeat_interleave(max_index, c, dim=1).view((b, c, 1))
+        flatten_feature = rearrange(feature, 'b c h w -> b c (h w)')
+        idx1 = torch.arange(b).view(-1, 1, 1)
+        idx2 = torch.arange(c).view(1, -1, 1)
+        params = flatten_feature[idx1, idx2, max_index]  # 取每个Batch和Channel下max_index所在值
+
+        xy, s, _ = params[:, 0:2, ...], params[:, 2:3, ...], params[:, 3:, ...]
+
+        xy_normal = torch.sigmoid(xy) - 0.5
+        s_normal = torch.exp2(self.M * (2 * torch.sigmoid(s) - 1))
+
+        for i in range(c):
+            xy_normal[:, i, ...] *= ori_shape[i + 2]  # 位移变换场需乘上原图的h w
+
+        s_flow, t_flow = self.get_st_flow(ori_shape, x.device)
+
+
+        return x
+
+
+        
+        
+        
+        
+        
+        
+
+if __name__ == '__main__':
+    input = torch.randn((4, 1, 512, 512))
+    model = TSFNet()
+    output = model(input)
+    
+
 
 
 
---
+++ models/flow.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as nnf
+import numpy as np
+
+
+class SpatialTransformer(nn.Module):
+    """
+    N-D Spatial Transformer
+    Obtained from voxelmorph
+    """
+
+    def __init__(self, size, mode='bilinear'):
+        super().__init__()
+
+        self.mode = mode
+
+        # create sampling grid
+        vectors = [torch.arange(0, s) for s in size]
+        grids = torch.meshgrid(vectors)
+        grid = torch.stack(grids)
+        grid = torch.unsqueeze(grid, 0)
+        grid = grid.type(torch.FloatTensor)
+
+        # registering the grid as a buffer cleanly moves it to the GPU, but it also
+        # adds it to the state dict. this is annoying since everything in the state dict
+        # is included when saving weights to disk, so the model files are way bigger
+        # than they need to be. so far, there does not appear to be an elegant solution.
+        # see: https://discuss.pytorch.org/t/how-to-register-buffer-without-polluting-state-dict
+        self.register_buffer('grid', grid)
+
+    def forward(self, src, flow):
+        # new locations
+        if self.grid.device != flow.device:
+            self.grid = self.grid.to(flow.device)
+
+        new_locs = self.grid + flow
+        shape = flow.shape[2:]
+
+        # need to normalize grid values to [-1, 1] for resampler
+        for i in range(len(shape)):
+            new_locs[:, i, ...] = 2 * (new_locs[:, i, ...] / (shape[i] - 1) - 0.5)
+
+        # move channels dim to last position
+        # also not sure why, but the channels need to be reversed
+        if len(shape) == 2:
+            new_locs = new_locs.permute(0, 2, 3, 1)
+            new_locs = new_locs[..., [1, 0]]
+        elif len(shape) == 3:
+            new_locs = new_locs.permute(0, 2, 3, 4, 1)
+            new_locs = new_locs[..., [2, 1, 0]]
+
+        return nnf.grid_sample(src, new_locs, align_corners=True, mode=self.mode)
+
+
+class VecInt(nn.Module):
+    """
+    Integrates a vector field via scaling and squaring.
+    Obtained from voxelmorph
+    """
+
+    def __init__(self, inshape, nsteps):
+        super().__init__()
+
+        assert nsteps >= 0, 'nsteps should be >= 0, found: %d' % nsteps
+        self.nsteps = nsteps
+        self.scale = 1.0 / (2 ** self.nsteps)
+        self.transformer = SpatialTransformer(inshape)
+
+    def forward(self, vec):
+        vec = vec * self.scale
+        for _ in range(self.nsteps):
+            vec = vec + self.transformer(vec, vec)
+        return vec
+
+
+def gen_flow_transport(shape_):
+    dim_ = len(shape_)
+
+    assert dim_ in [1, 2, 3]
+
+    xyz = [
+        torch.arange(i)
+        for i in shape_
+    ]
+
+    grid = torch.meshgrid(xyz)
+    id_grid = torch.stack(grid, 0).unsqueeze(0).type(torch.FloatTensor)
+
+    ones = torch.ones_like(id_grid)
+    ones.requires_grad = False
+    return ones
+
+
+def trans_t_flow(grid, t: torch.FloatTensor):
+    return grid * t
+
+
+def gen_flow_scale(shape_):
+    dim_ = len(shape_)
+
+    assert dim_ in [1, 2, 3]
+
+    xyz = [
+        torch.arange(i)
+        for i in shape_
+    ]
+
+    grid = torch.meshgrid(xyz)
+
+    mid = [
+        i // 2 for i in shape_
+    ]
+
+    mid = np.expand_dims(np.array(mid, dtype=np.float32), [0] + [i + 2 for i in range(dim_)])
+
+    id_grid = torch.stack(grid, 0).unsqueeze(0).type(torch.FloatTensor)
+    mid = torch.from_numpy(mid)
+
+    grid: torch.Tensor = -(id_grid - mid)
+
+    grid.requires_grad = False
+
+    return grid
+
+
+def trans_s_flow(grid, scale):
+    return grid * (1 - 1 / scale)
+
+
+if __name__ == '__main__':
+    import utils_medical.preproccess as prp
+    import utils_medical.plot as plot
+
+    image = prp.read_image("../data/bus.jpg").astype(np.float32)
+    # flow = gen_flow_scale(image.shape[:-1], 1)
+
+    ts_img = torch.from_numpy(image)[None, ...].permute(0, 3, 1, 2)
+    scale = torch.ones(1, 2, 1, 1).type(torch.FloatTensor) * 2
+    t = torch.tensor([200, 200])[None, :, None, None].type(torch.FloatTensor)
+
+    s_flow = trans_s_flow(gen_flow_scale(ts_img.shape[-2:]), scale)
+    t_flow = trans_t_flow(gen_flow_transport(ts_img.shape[-2:]), t)
+
+    sp_trans = SpatialTransformer(image.shape[:-1])
+    compose_flow = s_flow + sp_trans(t_flow, s_flow)
+
+    ts_new = sp_trans(ts_img, compose_flow)
+
+    np_new = ts_new.permute(0, 2, 3, 1).numpy()
+    np_img = ts_img.permute(0, 2, 3, 1).numpy()
+    np_flow = compose_flow.permute(0, 2, 3, 1).numpy()
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(np_new[0, :, ...] / 255)
+    plt.show()
+    plot.flow([np_flow[0, ::32, ::32, ...]], width=5)
